9aac9bb07ee7dca493740bb28ae2ecdc5c6de52f
David Morgan <david.alexandermorgan@gmail.com>
2018-08-30 19:37:32 +0000

    Recursive function working!

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if(fromN === toN){
+    return toN
+  }
+
+  return fromN + sum(fromN + 1, toN)
 }
+
+console.log(sum(0, 7));
 
 module.exports = sum;
